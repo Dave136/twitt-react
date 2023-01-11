38e07b2eb4611869e7bd650352e5605ac14f04fd
Dave136 <davejs136@gmail.com>
2023-01-10 23:06:16 -0400

    :tada: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="container">
+      <h2 className="text-2xl underline font-bold">Twitt</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1da0f2',
+        'primary-light': '#71c9f8',
+        'primary-grey': '#8799a6',
+        'primary-dark-light': '#192734',
+        'primary-dark': '#15212b',
+        'border-gray': '#38444d',
+        danger: '#e0245e',
+      },
+    },
+  },
+  plugins: [],
+};
